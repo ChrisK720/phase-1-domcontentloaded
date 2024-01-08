f0205bc4a2c0b0afbc24b7072cf6ee98a6b83c5d
Christopher Kirathe <chrisk720@Christophers-MacBook-Pro.local>
2024-01-09 02:51:24 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+function foo () {
+document.addEventListener('DOMContentLoaded', () =>{
+   const para = document.getElementById('text')
+   para.textContent = "This is really cool!"
+
+ })
+}
+foo()
